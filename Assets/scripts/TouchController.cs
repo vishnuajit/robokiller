7001c08f8585f07d0c025f74e5ba278c0717da84
vishnu-ajit <heart.beats5717@gmail.com>
2015-03-18 12:11:50 +0530

    touch detected on screen

---
+++ Assets/scripts/TouchController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchController : MonoBehaviour {
+
+public GUIText status;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.touchCount>0)
+		{
+			Touch touch = Input.GetTouch(0);
+			if(touch.phase==TouchPhase.Began)
+			{
+				status.text="\ntouched";
+			}
+			
+		}
+		else
+		{
+			
+			status.text="touch lifted";
+			
+		}
+	}
+}
