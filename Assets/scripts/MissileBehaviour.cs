1ecdfc447a2de4fbc1c09efd763ed8339b45a10c
vishnu-ajit <heart.beats5717@gmail.com>
2015-03-19 22:17:59 +0530

    plane shoots on touch

--- Assets/scripts/MissileBehaviour.cs
+++ Assets/scripts/MissileBehaviour.cs
@@ -5,38 +5,32 @@ public class MissileBehaviour : MonoBehaviour {
 
 	public Vector3 startPos;
 	public Vector3 targetPos;
+	public GUIText mstatus;
 	private float missileLifeTime;
 	private float journeyLength;
-	private float speed = 10.0f;
+	private float speed = 50.0f;
+	private float startTime = 0.0f;
 	// Use this for initialization
 	void Start () 
 	{
 		missileLifeTime =0.0f;
-		startPos = transform.position;
-		journeyLength = Vector3.Distance(startPos,targetPos);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-			missileLifeTime+=Time.deltaTime;
-		
-			Vector3 targetDirection = targetPos.normalized;
-			Vector3  shootForce = new Vector3(targetDirection.x * -10,targetDirection.y * 10 , targetDirection.z * 10);
-			//rigidbody.velocity = targetDirection * -200.0f;
-			rigidbody.AddForce(shootForce);
-			
-			
-			//float distCovered = (Time.time - missileLifeTime) * speed;
-			//float fracJourney = distCovered/journeyLength;
-		//	transform.position = Vector3.Lerp(startPos,targetPos,fracJourney);
+			startTime += Time.deltaTime	;	
+			float distCovered = startTime * speed;
+			journeyLength = Vector3.Distance(startPos,targetPos);
+			float fracJourney = distCovered/journeyLength;
+			transform.position = Vector3.Lerp(startPos,targetPos,fracJourney);
 			
 			if(missileLifeTime > 10.0f)
 			{
-				Debug.Log("missile destroyed"+this.name);
-				
+								
 				DestroyObject(this.gameObject);
+				missileLifeTime = 0.0f;
 				
 			}
 		
--- Assets/scripts/TouchController.cs
+++ Assets/scripts/TouchController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class TouchController : MonoBehaviour {
@@ -7,10 +7,10 @@ public GUIText status;
 public GameObject missile;
 public GameObject plane;
 public Transform testTransform;
+public GameObject bomb;
 
 private Rigidbody missileRB;
 private GameObject sphere;
-private Vector3 missileStartPos;
 private Vector3 missileStartRot;
 private float touchX;
 private float touchY;
@@ -19,27 +19,27 @@ private Vector3 touchPos;
 	void Start () 
 	{
 		status.text="init";
-		missileStartPos = new Vector3(-17.3f,0.0f,0.0f);
+		
 		missileStartRot = new Vector3(0.0f,90.0f,0.0f);
 		touchPos = new Vector3();
 		
 	}
 	
 	// Update is called once per frame
-	void firemissile()
+	void firemissile(Vector3 targetPos)
 	{
-		Debug.Log("touchpos"+touchPos);
-		//Vector3 worldPos = Camera.main.ScreenToWorldPoint(touchPos);
-		status.text="clicked"+touchPos;
-		Quaternion mrot = Quaternion.Euler(missileStartRot);
-		Vector3 mpos = new Vector3(missileStartPos.x,missileStartPos.y,plane.transform.position.z);
-		GameObject missileClone =(GameObject) Instantiate(missile,mpos,mrot);
-		Vector3 missileTargetPos = new Vector3(touchX,touchY,0.0f);
-		missileClone.GetComponent<MissileBehaviour>().targetPos = missileTargetPos;
-		
-	//	GameObject sphere = (GameObject)Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere),worldPos,Quaternion.identity);
-		//Vector3 spos = new Vector3(touchX,touchY,0);
-		//sphere.transform.position = worldPos;
+		Debug.Log("fire missile invoked");
+		Ray ray = Camera.main.ScreenPointToRay(targetPos);
+		RaycastHit hit;
+		if(Physics.Raycast(ray,out hit))
+		{
+			
+			status.text = "clicked sky at"+hit.point;
+			GameObject sphere = (GameObject)Instantiate(bomb,plane.transform.position,Quaternion.identity);
+			sphere.GetComponent<MissileBehaviour>().startPos = plane.transform.position;
+			sphere.GetComponent<MissileBehaviour>().targetPos = hit.point;
+				
+		}
 		
 	}
 	void Update () 
@@ -50,9 +50,9 @@ private Vector3 touchPos;
 			touchX = Input.mousePosition.x;
 			touchY = Input.mousePosition.y;
 			touchPos = new Vector3(touchX,touchY,0.0f);
-			firemissile();
+			firemissile(touchPos);
 		}
-		/*
+		
 		if(Input.touchCount>0)
 		{
 			Touch touch = Input.GetTouch(0);
@@ -60,17 +60,18 @@ private Vector3 touchPos;
 			{
 				touchX = touch.position.x;
 				touchY = touch.position.y;
-				firemissile();
+				touchPos = new Vector3(touchX,touchY,0.0f);
+				firemissile(touchPos);
 			}
 			
 		}
 		else
 		{
 			
-		//	status.text="touch lifted";
+			status.text="touch lifted";
 			
 		}
-		*/
+		
 	}
 	
 }
