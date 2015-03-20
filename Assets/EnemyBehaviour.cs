873a6c535645b0acdeacf4cfe910b92206c10701
vishnu-ajit <heart.beats5717@gmail.com>
2015-03-20 22:32:49 +0530

    spawned enemy and it comes towards our plane

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+
+	private GameObject plane;
+	private float startTime =0.0f;
+	private float journeyLength;
+	private float speed = 5.0f;
+	private Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+		plane = GameObject.Find("f16a");
+		startPos = transform.position;
+		
+		
+		float enemyX = Random.Range(-60.0f,15.0f);
+		float enemyY = 25.0f;
+		float enemyZ = 15.0f;
+		Vector3 enemyStartPos = new Vector3(enemyX,enemyY,enemyZ);
+		
+		startPos = enemyStartPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		startTime += Time.deltaTime	;	
+		float distCovered = startTime * speed;
+		journeyLength = Vector3.Distance(transform.position,plane.transform.position);
+		float fracJourney = distCovered/journeyLength;
+		transform.position = Vector3.Lerp(startPos,plane.transform.position,fracJourney);
+	
+	}
+}
---
+++ Assets/EnemyGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGenerator : MonoBehaviour {
+
+	private float currentTime;
+	// Use this for initialization
+	void Start () {
+		currentTime = 0.0f;
+		
+		generateEnemy();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		currentTime += Time.deltaTime;
+		
+		int timeRounded = (int)currentTime;
+		if(timeRounded%10==0)
+		{
+			//generateEnemy();
+			Debug.Log("enemy generated");
+		}
+	}
+	void generateEnemy()
+	{
+		GameObject enemy = (GameObject)Instantiate(GameObject.Find ("Enemy"),Vector3.zero,Quaternion.identity);
+	}
+}
